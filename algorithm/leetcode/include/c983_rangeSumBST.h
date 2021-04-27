103f223ff7b5a9b649e13168d5bff5de474a8569
xuruidong <xuruidong@gmail.com>
2021-04-27 13:15:28 +0800

    c983_rangeSumBST

---
+++ algorithm/leetcode/include/c983_rangeSumBST.h
@@ -0,0 +1,17 @@
+#ifndef C983_RANGESUMBST_H
+#define C983_RANGESUMBST_H
+
+
+class c983_rangeSumBST
+{
+    public:
+        c983_rangeSumBST();
+        virtual ~c983_rangeSumBST();
+        int rangeSumBST(TreeNode* root, int low, int high);
+
+    protected:
+
+    private:
+};
+
+#endif // C983_RANGESUMBST_H
