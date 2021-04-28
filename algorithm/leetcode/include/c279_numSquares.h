05814ba4ea452d254b21cb7fb2dd94f7c47e7f9e
xuruidong <xuruidong@gmail.com>
2021-04-28 18:50:48 +0800

    c279_numSquares

---
+++ algorithm/leetcode/include/c279_numSquares.h
@@ -0,0 +1,16 @@
+#ifndef C279_NUMSQUARES_H
+#define C279_NUMSQUARES_H
+
+
+class c279_numSquares
+{
+    public:
+        c279_numSquares();
+        virtual ~c279_numSquares();
+
+    protected:
+
+    private:
+};
+
+#endif // C279_NUMSQUARES_H
