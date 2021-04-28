cfac4d3294fb1468d7e9ee74492a088698f54ae7
xuruidong <xuruidong@gmail.com>
2021-04-28 18:02:19 +0800

    c633_judgeSquareSum

---
+++ algorithm/leetcode/include/c633_judgeSquareSum.h
@@ -0,0 +1,16 @@
+#ifndef C633_JUDGESQUARESUM_H
+#define C633_JUDGESQUARESUM_H
+
+
+class c633_judgeSquareSum
+{
+    public:
+        c633_judgeSquareSum();
+        virtual ~c633_judgeSquareSum();
+
+    protected:
+
+    private:
+};
+
+#endif // C633_JUDGESQUARESUM_H
