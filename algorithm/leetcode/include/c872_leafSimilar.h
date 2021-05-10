ed024f757c62385b4dbd609a5daf5cedfea25b0b
xuruidong <xuruidong@gmail.com>
2021-05-10 18:55:37 +0800

    c872_leafSimilar

---
+++ algorithm/leetcode/include/c872_leafSimilar.h
@@ -0,0 +1,16 @@
+#ifndef C872_LEAFSIMILAR_H
+#define C872_LEAFSIMILAR_H
+
+
+class c872_leafSimilar
+{
+    public:
+        c872_leafSimilar();
+        virtual ~c872_leafSimilar();
+
+    protected:
+
+    private:
+};
+
+#endif // C872_LEAFSIMILAR_H
