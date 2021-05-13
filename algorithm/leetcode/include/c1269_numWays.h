dea689c969c099959b0b3257932e4c22f234f0a2
xuruidong <xuruidong@gmail.com>
2021-05-13 23:20:52 +0800

    c1269_numWays

---
+++ algorithm/leetcode/include/c1269_numWays.h
@@ -0,0 +1,16 @@
+#ifndef C1269_NUMWAYS_H
+#define C1269_NUMWAYS_H
+
+
+class c1269_numWays
+{
+    public:
+        c1269_numWays();
+        virtual ~c1269_numWays();
+
+    protected:
+
+    private:
+};
+
+#endif // C1269_NUMWAYS_H
