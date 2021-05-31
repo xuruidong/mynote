907508355711f682ed35ecab1b04a54f6ead0394
xuruidong <xuruidong@gmail.com>
2021-05-31 18:28:59 +0800

    c342_isPowerOfFour

---
+++ algorithm/leetcode/include/c342_isPowerOfFour.h
@@ -0,0 +1,16 @@
+#ifndef C342_ISPOWEROFFOUR_H
+#define C342_ISPOWEROFFOUR_H
+
+
+class c342_isPowerOfFour
+{
+    public:
+        c342_isPowerOfFour();
+        virtual ~c342_isPowerOfFour();
+
+    protected:
+
+    private:
+};
+
+#endif // C342_ISPOWEROFFOUR_H
