4a05e29b1b198a68e30c608d5db8c7a06ed920df
xuruidong <xuruidong@gmail.com>
2021-06-01 18:45:28 +0800

    c1744_canEat

---
+++ algorithm/leetcode/include/c1744_canEat.h
@@ -0,0 +1,16 @@
+#ifndef C1744_CANEAT_H
+#define C1744_CANEAT_H
+
+
+class c1744_canEat
+{
+    public:
+        c1744_canEat();
+        virtual ~c1744_canEat();
+
+    protected:
+
+    private:
+};
+
+#endif // C1744_CANEAT_H
---
+++ algorithm/leetcode/src/c1744_canEat.cpp
@@ -0,0 +1,31 @@
+#include "c1744_canEat.h"
+
+c1744_canEat::c1744_canEat()
+{
+    //ctor
+}
+
+c1744_canEat::~c1744_canEat()
+{
+    //dtor
+}
+
+vector<bool> canEat(vector<int>& candiesCount, vector<vector<int>>& queries)
+{
+    vector<long long> sum;
+    sum.push_back(0);
+    for(int i=0; i<candiesCount.size(); ++i) {
+        sum.push_back(sum[i] + candiesCount[i]);
+    }
+
+    vector<bool> ans;
+    for(auto &q:queries) {
+        long long v1 = q[1] * q[2];
+        if(( v1 > sum[q[0]) || (q[2] <= sum[q[0]+1)) {
+            ans.push_back(true);
+        } else {
+            ans.push_back(false);
+        }
+    }
+    return ans;
+}
