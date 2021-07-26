d83737a79f9710edb4ef68c01308d7871aaf22e9
xuruidong <xuruidong@gmail.com>
2021-07-26 17:23:36 +0800

    c138_copyRandomList

---
+++ algorithm/leetcode/include/c138_copyRandomList.h
@@ -0,0 +1,16 @@
+#ifndef C138_COPYRANDOMLIST_H
+#define C138_COPYRANDOMLIST_H
+
+
+class c138_copyRandomList
+{
+    public:
+        c138_copyRandomList();
+        virtual ~c138_copyRandomList();
+
+    protected:
+
+    private:
+};
+
+#endif // C138_COPYRANDOMLIST_H
