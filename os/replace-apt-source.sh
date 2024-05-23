9efa879def7093130a13b899495955354f57af5d
徐瑞东 <xuruidong@gmail.com>
2024-05-23 16:18:51 +0800

    Update replace-apt-source.sh

--- os/replace-apt-source.sh
+++ os/replace-apt-source.sh
@@ -2,37 +2,27 @@
 
 CODENAME=`lsb_release -c | awk '{print $2}'`
 
-function append_str()
-{
-	sudo echo $1 >> /etc/apt/sources.list
-	#echo $1
-}
+CONTENT="
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/  $CODENAME-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-backports main restricted universe multiverse
 
-function clean_cfg()
-{
-	sudo echo "" > /etc/apt/sources.list
-}
- 
-clean_cfg
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-security main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-security main restricted universe multiverse
 
-append_str "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释"
-append_str "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME main restricted universe multiverse"
-append_str "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME main restricted universe multiverse"
-append_str "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-updates main restricted universe multiverse"
-append_str "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-updates main restricted universe multiverse"
-append_str "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/  $CODENAME-backports main restricted universe multiverse"
-append_str "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-backports main restricted universe multiverse"
-append_str ""
-append_str "# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-security main restricted universe multiverse"
-append_str "# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-security main restricted universe multiverse"
-append_str ""
-append_str "deb http://security.ubuntu.com/ubuntu/ $CODENAME-security main restricted universe multiverse"
-append_str "# deb-src http://security.ubuntu.com/ubuntu/ $CODENAME-security main restricted universe multiverse"
-append_str ""
-append_str "# 预发布软件源，不建议启用"
-append_str "# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-proposed main restricted universe multiverse"
-append_str "# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-proposed main restricted universe multiverse"
+deb http://security.ubuntu.com/ubuntu/ $CODENAME-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ $CODENAME-security main restricted universe multiverse
 
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $CODENAME-proposed main restricted universe multiverse
+"
+
+sudo echo "$CONTENT" > /etc/apt/sources.list
 
 sudo apt-get update
 
